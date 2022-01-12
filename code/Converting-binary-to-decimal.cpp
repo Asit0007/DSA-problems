72e9d8172f360b1d9d3dc50ff3c8874d2ccab61c
Asit <86393824+Asit0007@users.noreply.github.com>
2022-01-12 22:35:45 +0530

    Update and rename binary_to_decimal.cpp to Converting-binary-to-decimal.cpp

--- code/Converting-binary-to-decimal.cpp
+++ code/Converting-binary-to-decimal.cpp
@@ -6,13 +6,13 @@ int main() {
 
     int n,digit,i=0,ans=0;
     cin>>n;
-
+    //loop runs till all the numbers are being traversed
     while(n!=00) {
 
         digit = n%10;
 
         if(digit==1) {
-
+            
             ans = ans + (digit * pow(2,i));    
 
         }
