1b180cc3b946f7e792e7ebc23f969a3f499069fe
Asit <86393824+Asit0007@users.noreply.github.com>
2022-01-13 14:38:46 +0530

    Add files via upload

---
+++ code/replace_space_with_@40.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+string replaceSpaces(string &str){
+    
+	// Write your code here.
+    string temp="";
+    for(int i=0; i<str.length(); i++) {
+        if(str[i] == ' ') {
+            temp.push_back('@');
+            temp.push_back('4');
+            temp.push_back('0');
+        }
+        else {
+            temp.push_back(str[i]);
+        }
+    }
+    return temp;
+    
+}
+int main() {
+    string s="";
+    cout<<"\nEnter a string : ";
+    getline(cin,s);
+    cout<< replaceSpaces(s);
+}
